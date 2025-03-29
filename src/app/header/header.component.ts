37c6fba02d7ffeadc2e88a3377051471dc7beca2
Noe Valle <nouvalle.graf@gmail.com>
2025-03-29 06:59:16 -0300

    Portfolio de tarjetas
    Buena practica pero no definitivo

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, signal, effect } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  activeRoute = signal('');
 
+  constructor(private router: Router) {
+    effect(() => {
+      this.activeRoute.set(this.router.url);
+    });
+  }
+
+  isActive(route: string): boolean {
+    return this.activeRoute() === route;
+  }
 }
--- src/app/left-side/left-side.component.ts
+++ src/app/left-side/left-side.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
+
 @Component({
   selector: 'app-left-side',
   standalone: true,
